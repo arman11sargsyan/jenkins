8a63e61c9f47aa50f480220af0b2e49675008e16
arman <11armansargsyan@gmail.com>
2021-12-16 11:39:13 +0000

    task 3

--- task3.py
+++ task3.py
@@ -6,6 +6,7 @@ def get_Host_name_IP():
         host_ip = socket.gethostbyname(host_name)
         print("Hostname :  ",host_name)
         print("IP : ",host_ip)
+        print("The Jenkins server date when have been pushed code ") 
     except:
         print("Unable to get Hostname and IP")
  
